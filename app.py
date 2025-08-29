0eeb22e17112e51b38204deb15bea096fb7bfe0e
Janderson-Teles <janderson23w@gmail.com>
2025-08-28 22:01:56 -0300

    gitignore adicionado

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/sobre")
+def sobre():
+    return render_template('sobre.html')
+
+@app.route("/projetos")
+def projetos():
+    return render_template('projetos.html')
+
+@app.route("/contato")
+def contato():
+    return render_template('contato.html')
